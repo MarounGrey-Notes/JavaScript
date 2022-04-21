5737dbf824659ae17cc65f8de1f09b53a5df8be1
Maroun <62679040+maroungrey@users.noreply.github.com>
2022-04-20 23:39:35 -0700

    Update FizzBuzz.js

--- Challenges/FizzBuzz.js
+++ Challenges/FizzBuzz.js
@@ -1,1 +1,15 @@
 
+for (i=0; i<101; i++) {
+  if (i%15==0) {
+    return "fizzbuzz";
+  }
+  else if (i%3==0) {
+    return "fizz";
+  }
+  else if (i%5==0) {
+    return "buzz"
+  }
+  else {
+    return i;
+  }
+}
